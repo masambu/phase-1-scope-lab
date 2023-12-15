70fe08f0353ea22fa387783ff42baf78d4eecd31
Kelvin <kevohmasambu@gmail.com>
2023-12-15 12:51:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+
+var customerName;
+
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer() {
+  bestCustomer = 'new best customer';
+}
+
+
+const leastFavoriteCustomer = 'John Doe';
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane Doe'; 
+}
